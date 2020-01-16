21a4a208d2a1ed04b7a9266c0b75bf6f110fbf53
John Walker <jmgwalker@gmail.com>
2020-01-15 21:08:41 -0800

    Constants file without real values

---
+++ iexscripts/constants.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Author: J. Walker
+
+Static variables (constants)
+Replace these with real values
+
+"""
+
+# IEX
+IEX_TOKEN     = "XXXXXXXXXXXXXXXXX"
+
+# MongoDB
+MDB_USER      = "XXXXXXXXXXXXXXXXX"
+MDB_PASSWORD  = "XXXXXXXXXXXXXXXXX"
+MDB_HOST      = "XXXXXXXXXXXXXXXXX" 
+MDB_PORT      = 0000000000000000000 
+MDB_NAMESPACE = "XXXXXXXXXXXXXXXXX" 
