50b486b68a779f7d7b0d08b3c87ede20a10df085
John Walker <jmgwalker@gmail.com>
2020-01-23 15:46:11 -0800

    Extra inputs to setup including requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
 from setuptools import setup, find_packages
 
+required = [ 'requests',
+             'pandas',
+             'arrow',
+             'socketIO-client-nexus'
+             'pymongo',
+             'numpy',
+             'matplotlib',
+             'peewee',
+             'flask' ]
+
 setup(
     name='iexscripts',
     version='0.1.0',
+    description='Back-end processing for DIYWealth',
+    author='John Walker and Robert Stainforth',
+    url='https://github.com/DIYWealth',
     packages=find_packages('iexscripts'),
+    install_requires=required
 )
